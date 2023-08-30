7dcf709d00dd54eb2a7189d8b1f468ca3e998fba
Muhammed Salman <mahmadslman@gmail.com>
2023-08-30 23:36:01 +0300

    Add 'adapt modules to custom plugins'

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,10 +1,6 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("java-library")
-    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+    id("ss.android.library")
 }
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+android {
+    namespace = "com.ss.skillsync.domain"
 }
